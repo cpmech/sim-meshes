f5f765bc800811b21bd37206fe293eb6673fd23e
Dorival Pedroso <dorival.pedroso@gmail.com>
2023-09-10 09:28:10 +1000

    Add files

--- examples/quarter_ring_2d_qua8.rs
+++ examples/quarter_ring_2d_qua8.rs
@@ -4,10 +4,8 @@ use std::fmt::Write;
 
 const OUT_DIR: &'static str = "/tmp/sim-meshes";
 
-fn run(nr: usize, na: usize, global_max_area: f64, draw: bool) -> Result<(), StrError> {
-    let max_area = Some(global_max_area);
-
-    let mesh = Unstructured::quarter_ring_2d(3.0, 6.0, nr, na, false, max_area).unwrap();
+fn run(nr: usize, na: usize, draw: bool) -> Result<(), StrError> {
+    let mesh = Structured::quarter_ring_2d(3.0, 6.0, nr, na, GeoKind::Qua8).unwrap();
 
     let npoint = mesh.points.len();
     let ncell = mesh.cells.len();
@@ -16,7 +14,12 @@ fn run(nr: usize, na: usize, global_max_area: f64, draw: bool) -> Result<(), Str
     println!("ncell = {}", ncell);
 
     let mut key = String::new();
-    write!(&mut key, "{}/quarter_ring2d_{}points_{}cells", OUT_DIR, npoint, ncell).unwrap();
+    write!(
+        &mut key,
+        "{}/quarter_ring2d_qua8_{}points_{}cells",
+        OUT_DIR, npoint, ncell
+    )
+    .unwrap();
 
     let fn_vtu = [key.as_str(), ".vtu"].concat();
     let fn_msh = [key.as_str(), ".msh"].concat();
@@ -32,8 +35,8 @@ fn run(nr: usize, na: usize, global_max_area: f64, draw: bool) -> Result<(), Str
 }
 
 fn main() -> Result<(), StrError> {
-    run(50, 50, 0.01, true)?;
-    run(200, 200, 0.0001, false)?;
-    run(1000, 1000, 0.00001, false)?;
+    run(50, 50, true)?;
+    run(200, 200, false)?;
+    run(1000, 1000, false)?;
     Ok(())
 }
